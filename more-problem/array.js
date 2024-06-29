0e5b5c09c58c117bd9c568c98b81d7ff8f2e1901
Smart-imran <rst.imran4@gmail.com>
2024-06-29 23:46:31 +0600

    first commit

---
+++ more-problem/array.js
@@ -0,0 +1,8 @@
+const country ='Bangladesh';
+const age = 52;
+const isindependent = true;
+const student ={id:121, class: 9, Name:"Kasem"};
+const friends =[13,14,15,26,11,21];
+
+/* Array.isArray(friends */
+console.log(Array.isArray(friends));
