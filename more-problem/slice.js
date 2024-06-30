becad4231015488ef6596c1c20109b01ed7b9f29
Smart-imran <rst.imran4@gmail.com>
2024-06-30 11:21:27 +0600

    slice and splice

---
+++ more-problem/slice.js
@@ -0,0 +1,3 @@
+const friends = [12,45,32,22,44,62,29,69];
+const partials = friends.slice(0,3);
+console.log(partials);
---
+++ more-problem/splice.js
@@ -0,0 +1,7 @@
+const friends = [12,45,32,22,44,62,29,69];
+
+//Remove element  from array
+//return delete items
+const partials =friends.splice(1 ,4, 55,65,13,56);
+console.log(partials);
+console.log(friends);
