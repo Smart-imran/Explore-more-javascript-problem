cf3683de1f3efd103da6f190544120d65bc1506b
Smart-imran <rst.imran4@gmail.com>
2024-06-30 10:27:02 +0600

    third

--- more-problem/array.js
+++ more-problem/array.js
@@ -1,8 +1,14 @@
-const country ='Bangladesh';
+const country = 'Bangladesh';
 const age = 52;
 const isindependent = true;
-const student ={id:121, class: 9, Name:"Kasem"};
-const friends =[13,14,15,26,11,21];
+const student = { id: 121, class: 9, Name: "Kasem" };
+const friends = [13, 14, 15, 26, 11, 21];
 
 /* Array.isArray(friends */
 console.log(Array.isArray(friends));
+console.log(friends.includes(15));
+
+//concat
+const newfriendAge = [12, 13, 11, 24];
+let allFriendAge = newfriendAge.concat(friends);
+console.log(allFriendAge);
